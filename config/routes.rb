0e31e40c8291957b3c7efa2cf6475839bb09d726
ryotaro.saji <abcabcxxx01@gmail.com>
2020-03-17 10:15:26 +0000

    mikansei

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create] do
+  resources :users, only: [:index, :show, :new, :create, :edit, :update] do
     member do
       get :followings
       get :followers
